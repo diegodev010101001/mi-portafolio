6ba060364e9a718c7e8a8ea6b32dc1794d1e094f
diegodev010101001 <diegofdezkr@gmail.com>
2026-06-03 15:28:01 -0600

    Corregir configuración EmailJS

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -43,15 +43,15 @@ const Contacto = () => {
     try {
 
     await emailjs.send(
-      import.meta.env.VITE_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      import.meta.env.REACT_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.REACT_APP_EMAILJS_TEMPLATE_ID,
       {
       nombre: form.nombre,
       apellidos: form.apellidos,
       email: form.email,
       mensaje: form.mensaje,
       },
-    import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+    import.meta.env.REACT_APP_EMAILJS_PUBLIC_KEY
   );
 
   setSent(true);
