2c3b2d38561ce8dd3d87fd786ed1f6a5f30e23e7
diegodev010101001 <diegofdezkr@gmail.com>
2026-06-03 15:39:52 -0600

    Corregir variables de entorno de EmailJS

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -42,16 +42,16 @@ const Contacto = () => {
 
     try {
 
-    await emailjs.send(
-      import.meta.env.REACT_APP_EMAILJS_SERVICE_ID,
-      import.meta.env.REACT_APP_EMAILJS_TEMPLATE_ID,
-      {
+  await emailjs.send(
+    process.env.REACT_APP_EMAILJS_SERVICE_ID,
+    process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+    {
       nombre: form.nombre,
       apellidos: form.apellidos,
       email: form.email,
       mensaje: form.mensaje,
-      },
-    import.meta.env.REACT_APP_EMAILJS_PUBLIC_KEY
+    },
+    process.env.REACT_APP_EMAILJS_PUBLIC_KEY
   );
 
   setSent(true);
